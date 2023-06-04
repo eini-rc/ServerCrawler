e0ffd2c87e7c72c59de7443d277bfac283e5ec40
eini-rc <rc4140734@gmail.com>
2023-06-04 13:48:58 +0300

    feat(Task): generate basic project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    </head>
+    <body class="antialiased">
+       Laravel run successfully
+    </body>
+</html>
