9289816c7472545b7aa525de10557385aa27fe0c
eini-rc <rc4140734@gmail.com>
2023-06-06 17:41:15 +0300

    feat(Controller): add crawler controller layer

---
+++ app/Http/Controllers/CrawlerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CrawlerService;
+
+class CrawlerController extends Controller
+{
+    private $crawlerService;
+
+    public function __construct(CrawlerService $crawlerService)
+    {
+        $this->crawlerService = $crawlerService;
+    }
+
+    public function crawlAction(Request $request)
+    {
+        $url = $request->input('url');
+        $depth = $request->input('depth');
+
+        $results = $this->crawlerService->crawlUrls($url, $depth);
+
+        return response()->json($results);
+    }
+}
